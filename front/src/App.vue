37abf17433d1f795ceeba42726161c6cc07e6baa
Dichell <9792910@gmail.com>
2023-10-23 17:37:58 +0300

    forms & validation

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld />
+      <FormComponent />
     </v-main>
   </v-app>
 </template>
 
 <script setup lang="ts">
-  import HelloWorld from '@/components/HelloWorld.vue'
+  import FormComponent from '@/components/FormComponent.vue'
 </script>
---
+++ front/src/components/FormComponent.vue
@@ -0,0 +1,58 @@
+<template>
+    <v-container>
+        <v-row>
+            <v-col cols="6">
+                <form @submit.prevent="submit">
+
+                    <v-text-field 
+                        v-model="email.value.value" 
+                        :error-messages="email.errorMessage.value" 
+                        label="E-mail"
+                        >
+                    </v-text-field>
+
+                    <v-text-field 
+                        v-model="number.value.value" 
+                        :counter="6" 
+                        :error-messages="number.errorMessage.value"
+                        label="Number (optional)"
+                        >
+                    </v-text-field>
+
+                    <v-btn 
+                        type="submit"
+                        >submit
+                    </v-btn>
+
+                </form>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script setup lang="ts">
+import { useField, useForm } from 'vee-validate'
+
+const { handleSubmit, handleReset } = useForm({
+    validationSchema: {
+        email(value: string) {
+            const emailRegx = /\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/gi
+            if (emailRegx.test(value)) return true
+            return 'Must be a valid e-mail.'
+        },
+        number(value: number) {  
+            const numStr = value.toString()    
+            if (numStr.length == 0 || numStr.length == 6 ) return true
+            return 'Number should contains 6 digits.'
+        }
+    }
+})
+
+const email = useField('email')
+const number = useField('number')
+
+const submit = handleSubmit((values: any) => {
+    alert(JSON.stringify(values, null, 2))
+})
+
+</script>
