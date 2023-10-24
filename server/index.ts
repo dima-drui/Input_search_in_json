a3279443076e9ce19f143de225d9bb56d65d6e2f
Dichell <9792910@gmail.com>
2023-10-24 09:55:42 +0300

    logger

--- server/index.ts
+++ server/index.ts
@@ -1,20 +1,19 @@
 import express from 'express';
 import cors from 'cors';
 import UserController from './controllers/users';
+import morgan from 'morgan'
 
 const app = express();
 const port = 4000;
 
+app.use(morgan('common'))
 app.use(cors())
 app.use(express.json({ limit: 1024102420, type: "application/json" }));
+
 app.use(function(req,res,next){setTimeout(next,5000)});
 
 app.get('/users', UserController.search)
 
-const run = async () => {
-    app.listen(port, () => {
-        console.log(`Server running at: http://localhost:${port}`);
-    });
-}
-
-run()
+app.listen(port, () => {
+    console.log(`Server running at: http://localhost:${port}`);
+});
